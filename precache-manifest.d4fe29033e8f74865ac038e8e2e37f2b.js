64050903367824ba737fdc904e710d11d2a4a763
coolPreye <kerebipreye@gmail.com>
2019-11-13 17:21:59 -0800

    Updates

---
+++ precache-manifest.d4fe29033e8f74865ac038e8e2e37f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c65ab4bdbc375053cd18a7985f81da",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "8d597fe3fbdfc8772440",
+    "url": "/react-quiz-app/static/css/main.b06bb8a4.chunk.css"
+  },
+  {
+    "revision": "8b934cc52fbddddd8ff7",
+    "url": "/react-quiz-app/static/js/2.915a60af.chunk.js"
+  },
+  {
+    "revision": "8d597fe3fbdfc8772440",
+    "url": "/react-quiz-app/static/js/main.76e7e625.chunk.js"
+  },
+  {
+    "revision": "17aa4dd7a9b582aae027",
+    "url": "/react-quiz-app/static/js/runtime-main.2fcf9c79.js"
+  }
+]);
